db2c1ded18ba7bbc0aa44d4c12fffe08c5ffcd4e
noddy09 <n.mhapadi@gmail.com>
2021-07-17 18:17:15 +0530

    Added initial component in FE with routing

--- frontend/recommend-fe/src/app/app-routing.module.ts
+++ frontend/recommend-fe/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: "home", pathMatch: "full"},
+  {path: 'home', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
